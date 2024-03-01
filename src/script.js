fe5d4b1636c3477205fd336e3a6b5327a150761f
abinayasplatform <abinayaraja1624@gmail.com>
2024-03-01 22:57:41 +0530

    task16

---
+++ src/script.js
@@ -0,0 +1,53 @@
+setTimeout(() => {
+    document.getElementById("displayCountDown").innerText = "10";
+
+    setTimeout(() => {
+        document.getElementById("displayCountDown").innerText = "9";
+
+        setTimeout(() => {
+            document.getElementById("displayCountDown").innerText = "8";
+
+            setTimeout(() => {
+                document.getElementById("displayCountDown").innerText = "7";
+
+                setTimeout(() => {
+                    document.getElementById("displayCountDown").innerText = "6";
+
+                    setTimeout(() => {
+                        document.getElementById("displayCountDown").innerText = "5";
+
+                        setTimeout(() => {
+                            document.getElementById("displayCountDown").innerText = "4";
+
+                            setTimeout(() => {
+                                document.getElementById("displayCountDown").innerText = "3";
+
+                                setTimeout(() => {
+                                    document.getElementById("displayCountDown").innerText = "2";
+
+                                    setTimeout(() => {
+                                        document.getElementById("displayCountDown").innerText = "1";
+                                    
+                                        setTimeout(() => {
+                                            document.getElementById("displayCountDown").innerText = "Happy Independence Day!!!!";
+                                        
+                                        },2000);
+                                    },2000);
+                                
+                                },2000);
+                            
+                            },2000);
+                        
+                        },2000);
+                    
+                    },2000);
+                
+                },2000);
+            
+            },2000);
+        
+        },2000);
+    
+    },2000);
+
+},2000);
